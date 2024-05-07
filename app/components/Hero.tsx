1002e605d82d639c1519e1d0ad02ed8608212d67
nurrizkiamalia <rizkiamel9@gmail.com>
2024-05-08 06:53:24 +0700

    fix: image width and height for hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 const Hero: React.FC = () =>{
     return(
         <>
-        <div className="hero w-full h-[100vh] flex bg-lightgray z-0 mt-[-5.5rem] mb-20 relative justify-center items-center">
+        <div className="hero w-full flex bg-lightgray z-0 mt-[-5.5rem] pt-0 mb-20 relative justify-center items-center overflow-x-hidden">
             <div className="hero-hello">
                 <h2 className="hello text-heading3 translate-x-[70%] duration-1000 cursor-zoom-in hover:translate-x-0 transition-all ease-in-out absolute right-0 bg-offblack font-bold text-white py-5 px-[31px] rounded-l-full hover:shadow-xl hover:shadow-gray">👋<span className=" ml-10 ">Hi I'm Amalia</span></h2>
              </div>
@@ -14,12 +14,11 @@ const Hero: React.FC = () =>{
             <Image 
                 src="/assets/hero-img.png"
                 alt="Hero Image"
-                width={0}
-                height={0}
-                layout='responsive'
+                width={400}
+                height={400}
                 objectFit="cover"
                 objectPosition="center"
-                className="img-hero max-w-fit max-h-fit place-content-center align-middle bottom-0 absolute -z-10" />
+                className="img-hero w-fit place-content-center align-middle  -z-10" />
         </div>
         </>
     )
